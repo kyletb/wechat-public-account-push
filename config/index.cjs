03a4dd5d81c91d5a8f3b81b7c5218ef25f19a83a
kyletb <475753262@qq.com>
2023-09-09 23:01:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,7 +8,7 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxd7d61153ba4b279e',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oeyxL64MM2B_x3rhLLZtFz_FPqDE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'jord1TwdzH_QyvIISjJO3fpiHn6oaqL0DFyDV98j8ts',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'Ux1jR3ulZnu5ytv1SqH3hv_7fjTgG1H33TEDp16ipdU',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oeyxL64MM2B_x3rhLLZtFz_FPqDE',
     }
   ],
 
